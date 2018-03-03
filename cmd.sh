fe959bd1e3da0ec6a74c39e454648ac5ab41786b
Andy Shu <andy@symbol.is>
2018-03-03 19:31:52 +0800

    Add commands

---
+++ cmd.sh
@@ -0,0 +1,19 @@
+alias ts-node="./node_modules/.bin/ts-node"
+export NODE_PATH=src
+case $1 in
+  "fetch")
+    ts-node src/lexis/B-PageToParse/parsePage $2 refetch
+  ;;
+  "parse")
+    ts-node src/lexis/B-PageToParse/parsePage $2
+  ;;
+  "collect-lexes")
+    ts-node src/lexis/C-ParseToLexis/collectLexes.ts $2
+  ;;
+  "make-dict")
+    ts-node src/lexis/D-LexisToDict/makeInflectionDict.ts $2
+  ;;
+  "analyse-books")
+    ts-node src/corpus/analyseBooks.ts $2
+  ;;
+esac
