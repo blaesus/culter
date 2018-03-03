12c16032ce942359346c7b4c2c4818ba2ce9bc55
Andy Shu <andy@symbol.is>
2018-03-03 17:16:54 +0800

    Add control commands

--- cmd.sh
+++ cmd.sh
@@ -4,4 +4,10 @@ case $1 in
   "fetch")
     ts-node src/lexis/B-PageToParse/parsePage $2
   ;;
+  "analyse-books")
+    ts-node src/corpus/analyseBooks.ts
+  ;;
+  "make-dict")
+    ts-node src/lexis/D-LexisToDict/makeInflectionDict.ts
+  ;;
 esac
