6e5850b1f844b7b9caf565331213f01ea30c1e9a
Andy Shu <andy@symbol.is>
2018-03-03 17:52:16 +0800

    Add command to parse skip fetching

--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,11 @@
 alias ts-node="./node_modules/.bin/ts-node"
 export NODE_PATH=src
 case $1 in
-  "fetch")
+  "parse")
     ts-node src/lexis/B-PageToParse/parsePage $2
+  ;;
+  "fetch")
+    ts-node src/lexis/B-PageToParse/parsePage $2 refetch
   ;;
   "analyse-books")
     ts-node src/corpus/analyseBooks.ts
