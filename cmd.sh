2a789a7289270811ab9003bea6a829a12f4061da
Andy Shu <andy@symbol.is>
2018-03-03 18:45:59 +0800

    Rearrange command orders

--- cmd.sh
+++ cmd.sh
@@ -1,16 +1,19 @@
 alias ts-node="./node_modules/.bin/ts-node"
 export NODE_PATH=src
 case $1 in
-  "parse")
-    ts-node src/lexis/B-PageToParse/parsePage $2
-  ;;
   "fetch")
     ts-node src/lexis/B-PageToParse/parsePage $2 refetch
   ;;
-  "analyse-books")
-    ts-node src/corpus/analyseBooks.ts
+  "parse")
+    ts-node src/lexis/B-PageToParse/parsePage $2
+  ;;
+  "collect-lexes")
+    ts-node src/lexis/C-ParseToLexis/collectLexes.ts
   ;;
   "make-dict")
     ts-node src/lexis/D-LexisToDict/makeInflectionDict.ts
+  ;;
+  "analyse-books")
+    ts-node src/corpus/analyseBooks.ts $2
   ;;
 esac
