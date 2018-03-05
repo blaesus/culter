74f404506f514e376423fbb9711bfc9e7c2bead1
Andy Shu <andy@symbol.is>
2018-03-05 22:36:52 +0800

    Extract inflectionDict builder for each lexis

--- src/lexis/D-LexisToDict/makeInflectionDict.ts
+++ src/lexis/D-LexisToDict/makeInflectionDict.ts
@@ -2,45 +2,70 @@ import { database } from 'lexis/database'
 import { demacron, fallbackProxy } from 'utils'
 import { data } from 'lexis/data'
 import { MinimusSeriesStatus, parseSeriemStatus, serializeInflectedFormDesignation } from 'serialization'
+import { Lexis } from 'lexis'
 
 export type InflectionDict = {
     [forma in string]: MinimusSeriesStatus[]
 }
 
+function makeInflectionDict(): InflectionDict {
+    return fallbackProxy<InflectionDict>({}, () => [])
+}
+
+function extractInflectionDictFromLexis(
+    lexis: Lexis,
+    clavisWithMacron = false,
+): InflectionDict {
+    const inflectionDict = makeInflectionDict()
+    const inflectiones = Object.entries(lexis.inflectiones)
+    for (const inflectio of inflectiones) {
+        let [seriesStatus, formae] = inflectio
+        if (formae) {
+            for (const forma of formae) {
+                const clavis = clavisWithMacron ? forma : demacron(forma)
+                inflectionDict[clavis] = inflectionDict[clavis].concat(
+                    serializeInflectedFormDesignation(
+                        {
+                            lemma: lexis.lexicographia.lemma,
+                            pars: lexis.pars,
+                            forma,
+                            status: parseSeriemStatus(seriesStatus),
+                        }
+                    )
+                )
+            }
+        }
+    }
+    return inflectionDict
+}
+
+function mergeIntoDict(baseDict: InflectionDict, newDict: InflectionDict) {
+    for (const forma in newDict) {
+        baseDict[forma] = baseDict[forma].concat(newDict[forma])
+    }
+}
+
 async function makeDict(clavisWithMacron: boolean) {
-    await database.connect()
-    const inflectionDict = fallbackProxy<InflectionDict>({}, () => [])
+    const inflectionDict = makeInflectionDict()
     const lexesIds = await database.getLexesInternalIds()
     // Iterative style for performance reasons
     let index = 0
     for (const id of lexesIds) {
         const lexis = await database.getLexisByInternalId(id)
         if (!lexis) continue
-        const inflectiones = Object.entries(lexis.inflectiones)
         console.info(`${index++}/${lexesIds.length}`)
-        for (const inflectio of inflectiones) {
-            let [seriesStatus, formae] = inflectio
-            if (formae) {
-                for (const forma of formae) {
-                    const clavis = clavisWithMacron ? forma : demacron(forma)
-                    inflectionDict[clavis] = inflectionDict[clavis].concat(
-                        serializeInflectedFormDesignation(
-                            {
-                                lemma: lexis.lexicographia.lemma,
-                                pars: lexis.pars,
-                                forma,
-                                status: parseSeriemStatus(seriesStatus),
-                            }
-                        )
-                    )
-                }
-            }
-        }
+        const lexisDict = extractInflectionDictFromLexis(lexis, clavisWithMacron)
+        mergeIntoDict(inflectionDict, lexisDict)
     }
     await data.saveInflectionDict(inflectionDict)
     process.exit()
 }
 
-if (require.main === module) {
+async function main() {
+    await database.connect()
     makeDict(false)
+}
+
+if (require.main === module) {
+    main()
 }
